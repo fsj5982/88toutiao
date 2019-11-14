c4ae1bf4e51ba42760e06ccc166ca38752f90954
fsj5982 <598299360@qq.com>
2019-11-14 16:13:30 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,23 +8,23 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <el-form style="margin-top:30px">
-        <el-form-item>
+      <el-form ref="formobj" style="margin-top:30px" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
           <!-- 用户名框 -->
-          <el-input></el-input>
+          <el-input v-model="loginForm.mobile" placeholder="请输入您的手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- 验证码框 -->
-          <el-input style="width:270px"></el-input>
+          <el-input style="width:270px" v-model="loginForm.code" placeholder="请输入验证码"></el-input>
           <el-button plain style="float:right">获取验证码</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="checked">
           <!-- 勾选同意框 -->
-          <el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+          <el-checkbox v-model="loginForm.checked">我已阅读并同意用户协议及条款</el-checkbox>
         </el-form-item>
 
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button type="primary" style="width:100%" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -33,7 +33,40 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否勾选
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '请输入您的手机号' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号'
+        }], // 手机号
+        code: [{ required: true, message: '请输入您的验证码' }, {
+          pattern: /^\d{6}$/, message: '请输入正确的验证码'
+        }], // 验证码
+        checked: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('您还没有勾选用户协议'))
+          }
+        } // 是否勾选
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.formobj.validate(function (isok) {
+        if (isok) {
 
+        }
+      })
+    }
+  }
 }
 </script>
 
